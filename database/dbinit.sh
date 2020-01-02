5d22c656dce242595dd0f5116449e72ecf7bc560
Muhammad Usman <m_usmanayub@outlook.com>
2020-01-02 13:55:23 +0500

    updated database name in dinit.sh

--- database/dbinit.sh
+++ database/dbinit.sh
@@ -1,6 +1,6 @@
 PASS=edureka
 kubectl exec -it db-pod-0 -- mysql -uroot -p${PASS} <<MYSQL_SCRIPT
-CREATE DATABASE test2;
+CREATE DATABASE edureka;
 USE test2;
-create table test2(name varchar(20), email varchar(20));
+create table edureka(name varchar(20), email varchar(20));
 MYSQL_SCRIPT
